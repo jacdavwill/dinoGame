8323a0cc6f1466fc6283200a42a2695f8caff13b
jacdavwill <jacdavwill@gmail.com>
2020-06-25 21:52:09 -0600

    switch to dist from obj

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
 import pyautogui
-from random import uniform
-from time import time, sleep as sleeper
+import time
 
 # Navigate to chrome://dino and make the chrome window fill left half of screen.
 
@@ -11,56 +10,56 @@ keyboard_input = Controller()
 min_speed = 6
 max_speed = 13
 acceleration = .065  # speed/sec
-low = 430
-mid = 400
-startX = 100
-endX = 500
-stepX = 100
+# todo: make all points based on two corners only
+low = 440
+mid = 397
+startX = 70
+endX = 450
+width = endX - startX
+height = low - mid
 levels = 2
 ref = (500, 700)
-sites = []
-for i in range(startX, endX, stepX):
-    sites.append((i, low))
-    sites.append((i, mid))
 
 
-def get_key_name(key):
-    if isinstance(key, keyboard.KeyCode):
-        return key.char
-    else:
-        return str(key)
+def is_dark(pixel):
+    return pixel[0] < 100
 
 
-def (x, y, back_is_black):
-    color = pyautogui.pixel(x, y)
-    if (color[0] < 100 and back_is_black) or (color[0] > 100 and not back_is_black):
-        return 3
-    else:
-        return 10
+def has_obstacle(pixel, back_is_dark):
+    return is_dark(pixel) != back_is_dark
 
 
-def sleep(x):
-    sleeper(uniform(x, x))
+def get_obstacle_dist(img):
 
 
-def encode(site_spots, speed):
-    index = 0
-    for j in range(len(sites)):
-        if site_spots[j]:
-            index += pow(2, j)
-    return index * (speed - min_speed + 1)
+
+def load_q_table(num_states, all_actions, initial_val):
+    table = []
+    for state in range(num_states):
+        value = {}
+        for action in all_actions:
+            value[action] = initial_val
+        table.append(value)
+    return table
 
 
-actions = 3  # jump, duck, do nothing
-states = pow(2, levels * len(sites)) * (max_speed - min_speed)  # 1792=2^8*7
-print(encode([True,True,True,True,True,True,True,True], 7))
+NOTHING = 0
+DUCK = 1
+JUMP = 2
+actions = [NOTHING, DUCK, JUMP]
+states = levels *  1 # pow(2, levels * len(sites)) * (max_speed - min_speed + 1)  # 2040=2^8*8
+q_table = load_q_table(states, actions, 1.0)
 
+img = pyautogui.screenshot(region=(startX, mid, width, height))
 
+# print(sites[6])
 # start_time = time()
-# while True:
-#     start = time()
-#     background = pyautogui.pixel(ref[0], ref[1])[0] < 100
-#     site_vals = [get_color_val(site[0], site[1], background) for site in sites]
-#     site_vals.append(int(time() - start_time))
-#     print("delta: ", time() - start)
-#     sleep(1)
+while True:
+    # start = time.time()
+    #     print("fps: ", 1.0 / float(time() - start))
+    #     time.sleep(1)
+    img = pyautogui.screenshot()
+    img.crop()
+    print(get_obstacle_dist(img))
+    # print(pyautogui.position())
+    # time.sleep(.5)
